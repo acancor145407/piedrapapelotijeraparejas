37a19b2527da5fa9ab5953619bbeb92a1e2fa3ee
acancor145407 <98743495+acancor145407@users.noreply.github.com>
2022-02-09 03:09:21 -0500

    holi

--- main.ts
+++ main.ts
@@ -1,3 +1,60 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    jugar = randint(1, 3)
+    radio.sendNumber(jugar)
+    if (jugar == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (jugar == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
 })
+radio.onReceivedString(function (receivedString) {
+    jugar_2 = randint(1, 3)
+    if (jugar_2 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (jugar_2 == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+let jugar_2 = 0
+let jugar = 0
+radio.setGroup(1)
